b3601846e65d8a34c61a4356b3f7ba5778b0e090
graandda <graanddadays@gmail.com>
2022-11-13 21:00:27 +0200

    Removed insecure Django secret key from settings

--- film_service/settings.py
+++ film_service/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-iw(%%nc(149^o#om8itxz=el9#(re7_(3ru2^5wbg*vj_ft$ul")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
